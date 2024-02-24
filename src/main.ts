014eb66ed27a7e7b40e0189efd8df97393ef0800
Leonardorossato <leonardorossato21@gmail.com>
2024-02-24 11:18:25 -0300

    remoção de arquivos n utilizados, adição de novas implementações e começando po projeto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle('Pin Places Announcer API')
+    .setDescription('The Pin Places Announcer API documentation')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.APP_PORT || 3000, () =>
+    console.log(
+      `App is Running\nDocumentation available on http://localhost:${process.env.APP_PORT}/docs`,
+    ),
+  );
 }
-bootstrap();
+void bootstrap();
